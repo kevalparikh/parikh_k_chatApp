1db3f90747aba02ec674baeca61ad6465f43785a
kevalparikh50 <k_parikh3@fanshaweonline.ca>
2019-03-21 17:29:01 -0400

    adding build pack

---
+++ app.js
@@ -0,0 +1,20 @@
+// inisialize an express app and set it up
+const express = require('express');
+const app = express();
+const io = require('socket.io');
+
+//some config stuff
+const port = process.env.PORT || 3000;
+
+//tell our app to use the public folder for static files
+app.use(express.static('public'));
+
+//intialize only route we need
+app.get('/', (req, res, next)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+// create server variable for socket io
+const server = app.listen(port, () =>{
+  console.log(`app is running on port ${port}`)
+});
