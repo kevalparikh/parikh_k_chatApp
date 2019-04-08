cd94f08de2cee915f9a7cb888a2c8aab00763fe6
kevalparikh50 <k_parikh3@fanshaweonline.ca>
2019-04-07 23:01:01 -0400

    adding my project

--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -1,3 +1,4 @@
+
 import ChatMessage from './modules/ChatMessage.js';
 
 const socket = io();
@@ -18,10 +19,56 @@ function appendMessage(message) {
 
 const vm = new Vue({
     data: {
+        
+        connectedUsers: [],
         socketID: "",
         nickname: "",
         message: "",
         messages: []
+    },
+    
+    created: function (){
+        // if server emit user joined, update connectUser array
+         socket.on('user joined', function(socketId){
+             
+             //get already connected users
+             axios.get('/onlineusers')
+                    .then(function (response) {
+                        for(var key in response.data) {
+                            if(this.connectedUsers.indexOf(key) <= -1) {
+                                this.connectedUsers.push(key);
+                     }
+                   }
+                 console.log(this.connectedUsers); 
+             }.bind(this));
+             
+             
+                  var infoMsg = {
+                 "type": "info",
+                 "msg": "User " + socketId + " has Joined"
+             }
+             this.messages.push(infoMsg);
+            
+         }.bind(this));
+        
+        // if user left then run the array
+        
+        socket.on('user left', function (socketId) {
+                var index = this.connectedUsers.indexOf(socketId);
+                if(index >= 0) {
+                    this.connectedUsers.splice(index,1);
+                    
+                
+                  var infoMsg = {
+                 "type": "info",
+                 "msg": "User " + socketId + " has left"
+             }
+             this.messages.push(infoMsg);    
+                
+                }
+        }.bind(this));
+        
+        
     },
     
     methods: {
